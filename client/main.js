0f71fd4e20d8834b13e58b07067ff2186c9cf726
Rahul Gaur <rahul.nbg@gmail.com>
2014-11-09 23:27:34 +0530

    Removed unused subscriptions

--- client/main.js
+++ client/main.js
@@ -1,6 +1,4 @@
 Meteor.subscribe('posts');
-newPostsHandle = Meteor.subscribeWithPagination('newPosts', 10);
-topPostsHandle = Meteor.subscribeWithPagination('topPosts', 10);
 Meteor.autorun(function() {
     Meteor.subscribe('comment');
     Meteor.subscribe('comments', Session.get('currentPostId'));
--- server/publication.js
+++ server/publication.js
@@ -1,24 +1,6 @@
 Meteor.publish('posts', function() {
     return Posts.find({});
 });
-Meteor.publish('newPosts', function(limit) {
-    return Posts.find({}, {
-        sort: {
-            submitted: -1
-        },
-        limit: limit
-    });
-});
-Meteor.publish('topPosts', function(limit) {
-    return Posts.find({}, {
-        sort: {
-            votes: -1,
-            submitted: -1
-        },
-        limit: limit
-    });
-});
-
 Meteor.publish('notifications', function() {
     return Notifications.find({
         userId: this.userId
